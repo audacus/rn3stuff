a2ca870c037b5f13bf9cc5b9de8587dfd7c064c3
audacus <david.burkhart@hotmail.com>
2022-09-11 22:03:24 +0200

    add translation

--- src/language/translations/de-CH.js
+++ src/language/translations/de-CH.js
@@ -2,6 +2,9 @@ export default {
   welcome: 'Grüezi',
   title: {
     home: 'Deheime',
-    three_js: 'Fancy Züg'
+    three_js: 'Fancy Züg',
+  },
+  button: {
+    three_js: 'Zeig mir öppis cools',
   }
 }
